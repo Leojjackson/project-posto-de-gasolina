b749a7139bf5436d49e73a8e479d122124967ab5
Leojjackson <leojjackson@outlook.com>
2022-10-17 23:34:44 -0300

    Problema pequeno resolvido

--- criando.js
+++ criando.js
@@ -19,15 +19,24 @@
  }
 
  //  ---- funcionalidade dos textos digitados ----
- let cax1 = document.querySelector('#txt');
- let numero = document.querySelector("#num");
- let res = document.querySelector('#res');
- let caixa = cax1.value;
- let num = numero.value;
- function clicou(){
-  if(caixa == "gasolina"){
-     res.innerHTML = caixa;
- }else if(caixa == 'alcool' || caixa == ''){
-  res.innerHTML = caixa;
- }
- }
+ function clicou()
+ {
+   let cax1 = document.querySelector('#txt');
+   let numero = document.querySelector("#num");
+   let res = document.querySelector('#res');
+   let caixa = cax1.value;
+   let num = numero.value;
+ 
+      if(caixa == "gasolina" || caixa == 'Gasolina')
+   {
+      res.innerHTML = `Olá ${num} Você escolheu ${caixa}, prossiga para a bomba por favor!!!`
+   }
+      else if(caixa == 'alcool' || caixa == 'Alcool')
+   {
+      res.innerHTML = ` Olá ${num} Você escolheu ${caixa}, prossiga para a bomba por favor!!!`
+   }
+      else
+   {
+      window.alert('ERROR...as caixas ou uma delas ficaram vazias. ERROR..')
+   }
+ };
